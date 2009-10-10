dcd0b729cf1f1e82207ed61e8f0adcba69856684
rick <technoweenie@gmail.com>
2009-10-10 16:20:06 -0700

    heroku tweaks

---
+++ config/rackup.ru
@@ -0,0 +1,6 @@
+require File.dirname(__FILE__) + "/../demo/sparkline_demo.rb"
+
+set :run, false
+set :env, ENV['APP_ENV'] || :production
+
+run Sinatra::Application
--- demo/sparkline_demo.rb
+++ demo/sparkline_demo.rb
@@ -9,7 +9,7 @@ require 'rack-sparklines/cachers/filesystem'
 pub_dir = File.expand_path(File.join(File.dirname(__FILE__), 'public'))
 use Rack::Sparklines, :prefix => 'sparks',
   :handler => Rack::Sparklines::Handlers::CsvData.new(File.join(pub_dir, 'temps')),
-  :cacher  => Rack::Sparklines::Cachers::Filesystem.new(File.join(pub_dir, 'sparks'))
+  :cacher  => Rack::Sparklines::Cachers::Memory.new
 
 get '/' do
   @body = $readme
