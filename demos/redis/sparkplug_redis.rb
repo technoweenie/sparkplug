e12ae1f253d4e8f9760988ece1640a830d600be4
rick <technoweenie@gmail.com>
2009-12-12 14:30:27 -0800

    add sparkplug web view for redis demo

--- demos/redis/sparkplug_redis.rb
+++ demos/redis/sparkplug_redis.rb
@@ -17,7 +17,20 @@ use Sparkplug, :prefix => 'sparks',
   :cacher  => Sparkplug::Cachers::Filesystem.new(File.join(pub_dir, 'sparks'))
 
 get '/' do
-  'yo'
+  @plug_list = $redis_list.plug_list
+  erb :show
+end
+
+post '/modify' do
+  case params[:commit]
+    when /delete/i
+      $redis_list.find(params[:plug]).delete
+      redirect '/'
+    else
+      plug = $redis_list.find(params[:plug])
+      plug.add(params[:value])
+      redirect "/#{params[:plug]}"
+  end
 end
 
 post '/:plug/:value' do
@@ -32,12 +45,17 @@ get '/:plug.json' do
 end
 
 get '/:plug' do
-  plug = $redis_list.find(params[:plug])
-  plug.datapoints * ", "
+  @plug_list = $redis_list.plug_list
+  @plug      = $redis_list.find(params[:plug])
+  erb :show
 end
 
 delete '/:plug' do
   plug = $redis_list.find(params[:plug])
   plug.delete
   'deleted'
+end
+
+def h(s)
+  Rack::Utils.escape(s)
 end
